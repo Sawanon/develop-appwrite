b36abd5be49f240eece90702816f24b6a86e2f6f
Sawanon <sawanon01@hotmail.com>
2024-08-27 14:47:03 +0700

    change currentUrl

--- bank.js
+++ bank.js
@@ -3,7 +3,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 // export const currentUrl = "https://46af-110-170-209-198.ngrok-free.app"
-export const currentUrl = "http://demo.mylaos.lfie:3000"
+export const currentUrl = "http://demo.mylaos.life:3000"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const router = Router()
